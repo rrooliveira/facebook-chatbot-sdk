0aa14f8ca7699bc660fd280779500cc2eef6cc26
rodrigo.ruyoliveira <rodrigo.oliveira.globant@estadao.com>
2022-08-02 10:54:56 -0300

    Facebook Chatbot

--- src/Domain/Message/Services/WebHook.php
+++ src/Domain/Message/Services/WebHook.php
@@ -9,10 +9,6 @@ class WebHook
         $hubMode = filter_input(INPUT_GET, 'hub_mode');
         $hubVerifyToken = filter_input(INPUT_GET, 'hub_verify_token');
 
-        echo "Token =>   $token  " . PHP_EOL;
-        echo "Hub Mode => $hubMode " . PHP_EOL;
-        echo "Hub Verify Token => $hubVerifyToken " . PHP_EOL;
-
         if ($hubMode === 'subscribe' && $hubVerifyToken === $token) {
             return filter_input(INPUT_GET, 'hub_challenge');
         }
